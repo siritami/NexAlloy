a6006ffb6b5bd692068f0724cd45cf19e17821fa
XES SEX <117189041+gnadgnaoh@users.noreply.github.com>
2026-08-22 21:05:38 +0700

    Update ThreadsHook.kt

--- app/src/main/java/io/github/nexalloy/revanced/threads/ThreadsHook.kt
+++ app/src/main/java/io/github/nexalloy/revanced/threads/ThreadsHook.kt
@@ -1,11 +1,11 @@
 package io.github.nexalloy.revanced.threads
 
-// import io.github.nexalloy.revanced.threads.ads.HideAds
+import io.github.nexalloy.revanced.threads.ads.HideAds
 import io.github.nexalloy.revanced.threads.network.BlockNetwork
 import io.github.nexalloy.revanced.threads.tracking.SanitizeTrackingLinks
 
 val ThreadsPatches = arrayOf(
-  // HideAds,  
+  HideAds,  
   BlockNetwork, 
   SanitizeTrackingLinks,
 )
