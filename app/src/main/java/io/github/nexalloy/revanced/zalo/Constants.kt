7628bde7e54cffe802b249132bca3413ca188793
XES SEX <117189041+gnadgnaoh@users.noreply.github.com>
2026-08-22 21:08:18 +0700

    Create Constants.kt

---
+++ app/src/main/java/io/github/nexalloy/revanced/zalo/Constants.kt
@@ -0,0 +1,20 @@
+package io.github.nexalloy.revanced.zalo
+
+object AdtimaClasses {
+    const val ADTIMA = "Lcom/adtima/Adtima;"
+    const val ZADS_NATIVE = "Lcom/adtima/ads/ZAdsNative;"
+    const val ZADS_BUNDLE = "Lcom/adtima/ads/ZAdsBundle;"
+    const val ZADS_TRACKING = "Lcom/adtima/ads/ZAdsTracking;"
+}
+
+object ZaloFeedKeys {
+    const val TRACK_ADS = "track_ads"
+    const val ADS_DATA = "adsData"
+    const val ADS_TYPE = "adsType"
+    const val ADS_THUMB = "adsThumb"
+    const val ADS_URL = "adsUrl"
+    const val ADS_ACTION = "adsAction"
+    const val TEMPLATE_ID = "templateId"
+    const val ZINSTANT_FEED_INFO = "zinstant_feed_info"
+    const val UTYPE = "utype"
+}
